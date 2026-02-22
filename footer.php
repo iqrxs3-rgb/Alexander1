3d21a89d5e91e10246aad9568bfb0efa13b43f4d
Alexander Vel. <iqrxs3@gmail.com>
2026-02-22 12:28:42 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-20 border-t border-white/5 py-10 text-center">
+        <p class="text-gray-600 text-xs">© 2026 Ace Host. All rights reserved.</p>
+        <div class="mt-4 flex justify-center gap-4 text-gray-400">
+            <?php foreach($locations as $loc) { echo "<span class='text-[10px] uppercase'>$loc</span>"; } ?>
+        </div>
+    </footer>
+</body>
+</html>
